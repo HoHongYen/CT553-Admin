ba435671d4fe539aa0723cb4ae5588f0cd717afc
Ho Hong Yen <yenvt9a1@gmail.com>
2024-09-03 17:20:03 +0700

    Add upload product images

---
+++ src/components/ui/DivIcon.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const DivIcon = styled.div`
+  background: none;
+  border: none;
+  padding: 0.6rem;
+  border-radius: var(--border-radius-sm);
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: var(--color-grey-100);
+  }
+
+  & svg {
+    width: 2.2rem;
+    height: 2.2rem;
+    color: var(--color-brand-600);
+  }
+`;
+
+export default DivIcon;
--- src/pages/products/AddProduct.jsx
+++ src/pages/products/AddProduct.jsx
@@ -15,6 +15,7 @@ import { useCreateProduct } from "@/hooks/products/useCreateProduct";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import slugify from "slugify";
+import { HiCamera, HiPencil, HiTrash } from "react-icons/hi2";
 
 function AddProduct({ productToEdit = {} }) {
   const { createProduct, isLoading: isCreating } = useCreateProduct();
@@ -37,6 +38,27 @@ function AddProduct({ productToEdit = {} }) {
   const [categoryOptions, setCategoryOptions] = useState();
   const [category, setCategory] = useState();
   const [isUploadingImage, setIsUploadingImage] = useState(false);
+
+  const [images, setImages] = useState([
+    {
+      path: "https://tuongxinh.com.vn/wp-content/uploads/2024/02/z5122716454948_6df55452e093e488987ba4213857f458.jpg",
+    },
+    {
+      path: "https://res.cloudinary.com/dphzvfcmy/image/upload/v1725026756/CT553/canh_hong_view_ooezqt.png",
+    },
+    {
+      path: "https://tuongxinh.com.vn/wp-content/uploads/2023/12/3-2.jpg",
+    },
+    {
+      path: "https://tuongxinh.com.vn/wp-content/uploads/2023/12/6.jpg",
+    },
+    {
+      path: "https://tuongxinh.com.vn/wp-content/uploads/2023/12/4.jpg",
+    },
+    {
+      path: "https://tuongxinh.com.vn/wp-content/uploads/2023/12/maket-3-2.jpg",
+    },
+  ]);
 
   const [overview, setOverview] = useState("");
   const [material, setMaterial] = useState("");
@@ -138,6 +160,38 @@ function AddProduct({ productToEdit = {} }) {
     }
   }, [categories]);
 
+  const handleClickElement = (id) => {
+    document.getElementById(id).click();
+  };
+
+  const handleAddProductImage = (e) => {
+    const files = e.target.files;
+    setImages((images) => [
+      ...images,
+      {
+        file: files[0],
+        path: URL.createObjectURL(files[0]),
+      },
+    ]);
+  };
+
+  const handleEditProductImage = (e, index) => {
+    const files = e.target.files;
+    setImages((images) => {
+      const newImages = [...images];
+      newImages[index] = {
+        file: files[0],
+        path: URL.createObjectURL(files[0]),
+      };
+      return newImages;
+    });
+  };
+
+  const handleRemoveImage = (e, index) => {
+    e.preventDefault();
+    setImages((images) => images.filter((_, i) => i !== index));
+  };
+
   return (
     <>
       <Row type="horizontal">
@@ -146,7 +200,7 @@ function AddProduct({ productToEdit = {} }) {
       </Row>
       <Row>
         <Form onSubmit={handleSubmit(onSubmit)}>
-          <FormRow label="Tên sản phẩm" error={errors?.name?.message}>
+          <FormRow label="Tên sản phẩm:" error={errors?.name?.message}>
             <Input
               type="text"
               id="name"
@@ -160,11 +214,11 @@ function AddProduct({ productToEdit = {} }) {
               })}
             />
           </FormRow>
-          <FormRow label="Slug">
+          <FormRow label="Slug:">
             <Input type="text" id="slug" disabled value={slug} />
           </FormRow>
 
-          <FormRow label="Danh mục">
+          <FormRow label="Danh mục:">
             <TreeSelect
               style={{ width: "100%" }}
               value={category}
@@ -177,6 +231,65 @@ function AddProduct({ productToEdit = {} }) {
             />
           </FormRow>
 
+          <div className="mt-8 flex flex-col gap-4">
+            <label htmlFor="image" className="font-[500]">
+              Hình ảnh:
+            </label>
+            <div className="grid grid-cols-5 gap-2 border rounded p-4">
+              {images.map((image, index) => (
+                <div
+                  key={image.path}
+                  className="relative ol-span-1 flex items-center justify-center border-2 border-dashed border-slate-200"
+                >
+                  <div className="overflow-hidden">
+                    <img
+                      src={image.path}
+                      className="transition-all duration-700 hover:scale-105"
+                    />
+                  </div>
+                  <div className="cursor-pointer flex gap-1 absolute top-2 right-2">
+                    <HiPencil
+                      className="h-8 w-8"
+                      onClick={() =>
+                        handleClickElement(`editProductImage_${index}`)
+                      }
+                    />
+                    <HiTrash
+                      className="h-8 w-8"
+                      onClick={(e) => handleRemoveImage(e, index)}
+                    />
+                    <div className="absolute top-0 left-0 invisible">
+                      <input
+                        accept="image/*"
+                        type="file"
+                        id={`editProductImage_${index}`}
+                        onChange={(e) => handleEditProductImage(e, index)}
+                        disabled={isWorking}
+                      />
+                    </div>
+                  </div>
+                </div>
+              ))}
+
+              <div
+                onClick={() => handleClickElement("productImages")}
+                className="cursor-pointer relative col-span-1 w-[210px] h-[210px] flex items-center justify-center border-2 border-dashed border-slate-200"
+              >
+                <HiCamera className="h-14 w-14" />
+                <div className="absolute top-0 left-0 invisible">
+                  <input
+                    accept="image/*"
+                    type="file"
+                    id="productImages"
+                    multiple
+                    onChange={handleAddProductImage}
+                    disabled={isWorking}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+
           {/* add variant begin */}
           {/* add variant end */}
 
@@ -184,10 +297,10 @@ function AddProduct({ productToEdit = {} }) {
             {/* overview begin */}
             <div className="flex flex-col gap-4">
               <label htmlFor="overview" className="font-[500]">
-                Tổng quan sản phẩm
+                Tổng quan sản phẩm:
               </label>
               <Editor
-                data={material}
+                data={overview}
                 onChange={(event, editor) => {
                   setOverview(editor.getData());
                 }}
@@ -200,7 +313,7 @@ function AddProduct({ productToEdit = {} }) {
             {/* material begin */}
             <div className="flex flex-col gap-4">
               <label htmlFor="material" className="font-[500]">
-                Chất liệu tranh
+                Chất liệu tranh:
               </label>
               <Editor
                 data={material}
@@ -216,7 +329,7 @@ function AddProduct({ productToEdit = {} }) {
             {/* specification begin */}
             <div className="flex flex-col gap-4">
               <label htmlFor="specification" className="font-[500]">
-                Thông tin chi tiết
+                Thông tin chi tiết:
               </label>
               <Editor
                 data={specification}
@@ -232,7 +345,7 @@ function AddProduct({ productToEdit = {} }) {
             {/* instruction begin */}
             <div className="flex flex-col gap-4">
               <label htmlFor="instruction" className="font-[500]">
-                Hướng dẫn vệ sinh tranh
+                Hướng dẫn vệ sinh tranh:
               </label>
               <Editor
                 data={instruction}
